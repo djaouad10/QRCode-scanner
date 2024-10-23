8ab6e10a4b72dd4af945780e34d760e0eb58c2bc
Gharbi djaouad <djaouadgharbi10@gmail.com>
2024-10-23 20:21:33 +0100

    edited login routes

--- controllers/login.js
+++ controllers/login.js
@@ -5,10 +5,26 @@ const logInStudent = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      res
-        .status(400)
-        .json({ success: false, message: "Please provide all credentials" });
+      throw new InvalidCredentialsErr("Please provide all credentials");
     }
+    const students = await pool.query(
+      "SELECT * FROM students WHERE email = $1",
+      [email]
+    );
+
+    if (!students.rows[0]) {
+      throw new NotFoundErr("No user was found with this email");
+    }
+
+    if (password !== students.rows[0].hashedpassword) {
+      throw new InvalidCredentialsErr("Invalid password");
+    }
+
+    req.session.user = students.rows[0];
+    res.status(200).json({
+      success: true,
+      message: `$Welcom ${students.rows[0].firstname}`,
+    });
   } catch (error) {
     console.log(error);
     next(error);
@@ -21,23 +37,24 @@ const logInTeacher = async (req, res, next) => {
     if (!email || !password) {
       throw new InvalidCredentialsErr("Please provide all credentials");
     }
-    const teachers = pool.query("SELECT * FROM teachers WHERE email = $1", [
-      email,
-    ]);
-    if (!teachers[0]) {
-      throw new NotFoundErr("No user found with this email");
+    const teachers = await pool.query(
+      "SELECT * FROM teachers WHERE email = $1",
+      [email]
+    );
+    if (!teachers.rows[0]) {
+      throw new NotFoundErr("No user was found with this email");
     }
 
-    if (password !== teachers[0].hashedpaswword) {
+    if (password !== teachers.rows[0].hashedpassword) {
       throw new InvalidCredentialsErr("Invalid password");
     }
 
-    res.session.user = teachers[0];
-    res
-      .status(200)
-      .json({ success: true, message: `$Welcom {teachers[0].firstname}` });
+    req.session.user = teachers.rows[0];
+    res.status(200).json({
+      success: true,
+      message: `$Welcom ${teachers.rows[0].firstname}`,
+    });
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
